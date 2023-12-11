fd713caecc270a027be0964e20109b34109f6799
Cometrue <96596469+ComeTrue666666@users.noreply.github.com>
2023-12-11 04:26:11 -0500

    Update Tree.h

--- DecisionTree/Tree.h
+++ DecisionTree/Tree.h
@@ -2,18 +2,26 @@
 #include<vector>
 #include<string>
 #include"Node.h"
+#include<queue>
+#include<iostream>
+#include<map>
+#include<cmath>
 
 class DecisionTree {
 
 private:
-	Node *root;
 
 public:
+	Node* root;
 	DecisionTree();
 	DecisionTree(Node* node);
 	void setRoot(Node* value);
 	//void expandChild(Node* node);
-	void buildTree(vector < vector<string>> dataset, int atributeSize);
-
-
+	void buildTree(vector < vector<string>> &dataset, string majorityClass);
+	Node* getRoot();
+	void printTree();
+	map<string, int> findClasses(vector<vector<string>>& database);
+	string findMajority(map<string, int>& table);
+	float computeGini(map<string, int> distribution);
+	float weightedGini(int totalSample, float leftGini, float rightGini, int leftSample, int rightSample);
 };
