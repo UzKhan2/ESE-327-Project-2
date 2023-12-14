aa178424f434a4fa71986ab2d97554b4942e3fda
Cometrue <96596469+ComeTrue666666@users.noreply.github.com>
2023-12-13 22:22:04 -0500

    Update Tree.h

--- DecisionTree/Tree.h
+++ DecisionTree/Tree.h
@@ -6,6 +6,7 @@
 #include<iostream>
 #include<map>
 #include<cmath>
+#include<sstream>
 
 class DecisionTree {
 
@@ -17,11 +18,16 @@ public:
 	DecisionTree(Node* node);
 	void setRoot(Node* value);
 	//void expandChild(Node* node);
-	void buildTree(vector < vector<string>> &dataset, string majorityClass);
+	void buildTree(Node* node, vector < vector<string>> &dataset);
 	Node* getRoot();
 	void printTree();
 	map<string, int> findClasses(vector<vector<string>>& database);
 	string findMajority(map<string, int>& table);
 	float computeGini(map<string, int> distribution);
-	float weightedGini(int totalSample, float leftGini, float rightGini, int leftSample, int rightSample);
+	float getWeightedGini(float leftGini, float rightGini, int leftSample, int rightSample);
+	map<string, float> findBestSplit(vector<vector<string>>& dataset, int featureNum);
+	vector<float> findColumnRange(vector<vector<string>>& dataset, int column_index);
+	float findStep(float gap);
+	int getSum(map<string, int> distribution);
+	bool digitCheck(string str);
 };
